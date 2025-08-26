912577a478a1102fe87eaed39dff93221b6e4543
Artem Dramaretskii <a.dramaretskii@visionlabs.ai>
2025-08-26 18:18:44 +0300

    Fix login link test

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,9 +1,15 @@
-from Course_project.pages.main_page import MainPage
+from pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # теперь сигнатура совпадает
+    page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
