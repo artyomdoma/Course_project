d0fa836a1bd3a7dfc0f56b2f125c21a16c0333f8
Artem Dramaretskii <a.dramaretskii@visionlabs.ai>
2025-08-27 14:03:25 +0300

    Add login_page

---
+++ pages/login_page.py
@@ -0,0 +1,29 @@
+from .base_page import BasePage
+from selenium import webdriver
+from .locators import LoginPageLocators, MainPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.go_to_login_page()
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        return LoginPage(browser=self.browser, url=self.browser.current_url)
+
+    def should_be_login_url(self):
+        current = self.browser.current_url
+        assert "login" in current, \
+            f"Login page did not contain 'login' in {self.browser.current_url}"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), \
+            f"Login form did not contain 'login' in {self.browser.current_url}"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), \
+            f"Register form did not contain 'register' in {self.browser.current_url}"
